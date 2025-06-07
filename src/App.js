3ae854f77e3f8282d11857c16363c00d2fa4c2c1
karanparwani <karan.p@universalassets.com>
2025-06-07 17:49:34 +0400

    basic app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AdminLoginForm from "./components/AdminLoginForm";
+import PatientDetails from "./components/PatientDetails";
+import AddPatientPage from "./components/AddPatientPage";
+import UpdatePatient from "./components/UpdatePatient";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<AdminLoginForm />} />
+        <Route path="/patients" element={<PatientDetails />} />
+        <Route path="/add-patient" element={<AddPatientPage />} />
+        <Route path="/update-patient/:id" element={<UpdatePatient />} />
+      </Routes>
+    </Router>
   );
 }
 
